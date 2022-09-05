1f45804ff1c91f735766d06c334adf851cb6efbb
Ankit Shubham <ankitshubham97@gmail.com>
2022-09-05 12:13:36 +0530

    fix: only use des encryption

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -12,14 +12,6 @@ type AlgoList = {
 }
 
 export const algoList: AlgoList = {
-    'blowfish': {
-        keyLength: 32,
-        ivLength: 8,
-    },
-    'aes-256-ctr': {
-        keyLength: 32,
-        ivLength: 16,
-    },
     'des': {
         keyLength: 8,
         ivLength: 8,
