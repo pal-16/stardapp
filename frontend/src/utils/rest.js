dadd63a57aee56bc9db83bb5ad4bd1e9138d0474
Ankit Shubham <ankitshubham97@gmail.com>
2022-09-11 02:08:54 +0530

    feat: working frontend

---
+++ frontend/src/utils/rest.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { API_BASE_URL, PACKED_NONCE } from '../constants';
+
+export const getAccessToken = async ({signature, walletPublicKey, walletPublicAddress}) => {
+  try{
+    const res = await axios.post(
+      `${API_BASE_URL}/token`, {
+        nonce: PACKED_NONCE,
+        signature,
+        walletPublicKey,
+        walletPublicAddress,
+      },
+      {
+        withCredentials: true,
+      }
+    );
+    console.log(res.data);
+    return { accessToken: res.data.accessToken, error: null };
+  } catch (error) {
+    return { accessToken: null, error };
+  }
+};
+
+export const removeAccessToken = async () => {
+  const res = await axios.delete(
+    `${API_BASE_URL}/token`, {
+      withCredentials: true,
+    }
+  );
+  console.log(res.data);
+  return res.data;
+}
+
+export const getContent = async () => {
+  try {
+    const res = await axios.post(
+      `${API_BASE_URL}/download`,
+      {
+        "filename": "rick-roll-rick-ashley.gif"
+      },
+      {
+        responseType: 'blob',
+        withCredentials: true
+      }
+    );
+    return {uri: URL.createObjectURL(res.data), error: null};
+  } catch (err) {
+    return {uri: null, error: err};
+  }
+}
