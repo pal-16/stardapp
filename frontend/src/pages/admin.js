121ec5a3bd4e7d49c5d6051e250f8f537c953bc2
Ankit Shubham <ankitshubham97@gmail.com>
2022-09-12 02:53:52 +0530

    fix: beautify admin page

--- frontend/src/pages/admin.js
+++ frontend/src/pages/admin.js
@@ -6,96 +6,108 @@ import { API_BASE_URL, PACKED_NONCE } from '../constants';
 class Admin extends Component {
 
 	state = {
-
-	// Initially, no file is selected
-	selectedFile: null
+	  selectedFile: null,
+    init: true
 	};
+
+  componentDidMount() {
+    if (this.state.init) {
+      this.setState({ init: false });
+      prompt('Enter Admin credentials twice', '');
+    }
+  }
 	
 	// On file select (from the pop up)
 	onFileChange = event => {
-	
-	// Update the state
-	this.setState({ selectedFile: event.target.files[0] });
-	
+	  // Update the state
+	  this.setState({ selectedFile: event.target.files[0] });
 	};
 	
 	// On file upload (click the upload button)
 	onFileUpload = () => {
-	
-	// Create an object of formData
-	const formData = new FormData();
-	
-	// Update the formData object
-	// formData.append(
-	// 	"myFile",
-	// 	this.state.selectedFile,
-	// 	this.state.selectedFile.name
-	// );
-  formData.append('key', '12345678');
-  formData.append('salt', '12345678');
-  formData.append('algo', 'des');
-  formData.append('file', this.state.selectedFile);
-	
-	// Details of the uploaded file
-	console.log(this.state.selectedFile);
-	
-	// Request made to the backend api
-	// Send formData object
-	axios.post(`${API_BASE_URL}/upload`, formData);
+    // Create an object of formData
+    const formData = new FormData();
+    
+    // Update the formData object
+    // formData.append('key', '12345678');
+    // formData.append('salt', '12345678');
+    // formData.append('algo', 'des');
+    formData.append('file', this.state.selectedFile);
+    
+    // Details of the uploaded file
+    console.log(this.state.selectedFile);
+    
+    // Request made to the backend api
+    // Send formData object
+    axios.post(`${API_BASE_URL}/upload`, formData);
 	};
 	
 	// File content to be displayed after
 	// file upload is complete
 	fileData = () => {
-	
-	if (this.state.selectedFile) {
-		
-		return (
-		<div>
-			<h2>File Details:</h2>
-			
-<p>File Name: {this.state.selectedFile.name}</p>
-
-			
-<p>File Type: {this.state.selectedFile.type}</p>
-
-			
-<p>
-			Last Modified:{" "}
-			{this.state.selectedFile.lastModifiedDate.toDateString()}
-			</p>
-
-		</div>
-		);
-	} else {
-		return (
-		<div>
-			<br />
-			<h4>Choose before Pressing the Upload button</h4>
-		</div>
-		);
-	}
+    if (this.state.selectedFile) {
+      return (
+        <p>
+          File Name: {this.state.selectedFile.name}
+          <br/>
+          File Type: {this.state.selectedFile.type}
+          <br/>
+          Last Modified:{" "}
+            {this.state.selectedFile.lastModifiedDate.toDateString()}
+        </p>
+      );
+    }
 	};
+
+  inited = () => {
+    this.setState({ init: false });
+  }
 	
 	render() {
-	
-	return (
-		<div>
-			<h1>
-			GeeksforGeeks
-			</h1>
-			<h3>
-			File Upload using React!
-			</h3>
-			<div>
-				<input type="file" onChange={this.onFileChange} />
-				<button onClick={this.onFileUpload}>
-				Upload!
-				</button>
-			</div>
-		{this.fileData()}
-		</div>
-	);
+    return (
+      <div>
+        <div className="navbar navbar-dark bg-warning fixed-top">
+          <div className="container py-2">
+            <a href="/" className="navbar-brand">
+              Fanstop Demo
+            </a> 
+            <a href="https://github.com/ankitshubham97/fanstop">
+              <img src="github-logo-6531.png" alt="public-address" className="ml-2" />
+            </a>
+          </div>
+        </div>
+        <section className="intro">
+          <div className="mask mt-2 d-flex align-middle align-items-center h-100">
+            <div className="container"  style={{ marginTop: "100px"}}>
+              <div className="row justify-content-center">
+                <div className="col-12">
+                  <div className="card">
+                    <div className="card-body p-0">
+                      <center>
+                          <div style={{ height: "400px", paddingTop: "100px"}}>
+                            <p className="fw-bold">
+                              Upload your new content to IPFS with encryption now!
+                            </p>
+                            <div>
+                              <input type="file" onChange={this.onFileChange} />
+                              <button onClick={this.onFileUpload}>
+                              Encrypt and Upload!
+                              </button>
+                            </div>
+                            <div>     
+                              {this.fileData()}
+                            </div>
+                          </div>
+                      </center>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
 	}
 }
 
