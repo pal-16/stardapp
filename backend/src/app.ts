dd85d3b895729f47b00d3a36e605f24414137075
Ankit Shubham <ankitshubham97@gmail.com>
2022-10-11 19:21:42 +0530

    fix: CORS issue

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -38,15 +38,25 @@ class App {
       })
     );
     this.app.use(fileUpload());
-    this.app.use(cors({origin: ['http://localhost:3001',
-    'https://dappstar-app.vercel.app',
-    'http://dappstar-app.vercel.app',
-    'https://dappstar-app.vercel.app/',
-    'http://dappstar-app.vercel.app/',
-    'https://dappstar.vercel.app',
-    'http://dappstar.vercel.app',
-    'https://dappstar.vercel.app/',
-    'http://dappstar.vercel.app/'], credentials: true}));
+    this.app.use(cors({origin: ['https://dappstar-app.vercel.app'], credentials: true}));
+    // this.app.use(function (req, res, next) {
+
+    //   // Website you wish to allow to connect
+    //   res.setHeader('Access-Control-Allow-Origin', 'https://dappstar-app.vercel.app');
+  
+    //   // Request methods you wish to allow
+    //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+    //   // Request headers you wish to allow
+    //   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+    //   // Set to true if you need the website to include cookies in the requests sent
+    //   // to the API (e.g. in case you use sessions)
+    //   res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+    //   // Pass to next layer of middleware
+    //   next();
+    // });
   }
 
   private initializeControllers(controllers: readonly Controller[]) {
