a8cce1b36811f83381cf2fdafa80f826542e1bd6
Ankit Shubham <ankitshubham97@gmail.com>
2022-10-14 04:47:10 +0530

    fix: correct api uri

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = "https://stardapp-backend.herokuapp.com";
-export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "https://stardapp.herokuapp.com";
+// export const API_BASE_URL = "http://localhost:3000";
 export const NFT_CONTRACT_ADDRESS = "0x23278452a4847B5fc6dD23A49fc6Aa70F9226fBb";
 export const MINT_PRICE = 1;
 export const COINEX_TESTNET_EXPLORER="https://testnet.coinex.net";
